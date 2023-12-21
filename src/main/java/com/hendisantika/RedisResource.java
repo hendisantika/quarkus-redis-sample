2cc703752649389a6c628f0a063e6d5f9d6c4cc8
hendisantika <hendisantika@yahoo.co.id>
2023-12-21 07:45:04 +0700

    Add delete in RedisResource 🫸🌀✏️📗:octocat:🐧🐳

--- src/main/java/com/hendisantika/RedisResource.java
+++ src/main/java/com/hendisantika/RedisResource.java
@@ -2,10 +2,7 @@ package com.hendisantika;
 
 import io.smallrye.mutiny.Uni;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.PUT;
-import jakarta.ws.rs.Path;
+import jakarta.ws.rs.*;
 
 import java.util.List;
 
@@ -51,5 +48,11 @@ public class RedisResource {
     @Path("/{key}")
     public void increment(String key, Integer value) {
         service.increment(key, value);
+    }
+
+    @DELETE
+    @Path("/{key}")
+    public Uni<Void> delete(String key) {
+        return service.delete(key);
     }
 }
