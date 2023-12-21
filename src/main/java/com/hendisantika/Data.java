10a93b5a89d9143cf2a6d50cc8386fb21338f8de
hendisantika <hendisantika@yahoo.co.id>
2023-12-21 07:37:49 +0700

    Add Data Record🫸🌀✏️📗:octocat:🐧🐳

---
+++ src/main/java/com/hendisantika/Data.java
@@ -0,0 +1,14 @@
+package com.hendisantika;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : quarkus-redis-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/21/23
+ * Time: 07:37
+ * To change this template use File | Settings | File Templates.
+ */
+public record Data(String key, int value) {
+}
